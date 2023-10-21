5bdabfe5614376ff30af74434fd81ebc209474c4
bhagirathpaliyal <bhagirathpaliya@gmail.com>
2023-10-21 21:09:02 +0530

    create page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from "react";
+import "./App.css"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [color,setColor]=useState("")
+    const data = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray", "turquoise", "lavender", "crimson", "magenta"];
+
+
+    return(
+        <div style={{backgroundColor:color,display:"flex",justifyContent:"center",alignItems:"center",width:"100%",height:"100vh"}}>
+
+<div style={{display:"flex"}}>
+    {data.map((value)=>(
+        
+        <button className="button" onClick={()=>(setColor(value))} style={{backgroundColor:value}}>{value}</button>
+    ))}
+</div>
+
+            
+        </div>
+    )
 }
 
 export default App;
